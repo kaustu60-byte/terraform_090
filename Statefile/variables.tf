4440c3f677ea50b7940ed463144d35d2919498d4
kaustubh <kaustubha2000wavare@gmail.com>
2026-03-10 11:33:16 +0530

    added a terraform fileS

---
+++ Statefile/variables.tf
@@ -0,0 +1,19 @@
+variable "region_name" {
+    default = ""
+  
+}
+variable "access_key" {
+    default = ""
+  
+}
+variable "secret_key" {
+  default = ""
+
+}
+variable "ami_id" {
+    default = ""
+  
+}
+variable "instance_type" {
+  default = ""
+}
